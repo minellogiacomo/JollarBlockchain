cf36d693266ffd77b08b8dfce4145664128ed0b6
Giacomo Minello <minellogiacomo@gmail.com>
2018-05-10 18:44:14 +0200

    still doesn't workk

--- keygenerator.java
+++ keygenerator.java
@@ -1,24 +1,22 @@
-package gensig;
+package example;
 
 import jolie.runtime.JavaService;
-import Jolie.runtime.Value;
+import jolie.runtime.Value;
 import java.security.*;
 
 public class GenSig extends JavaService {
 
-    public Value KeyGenerator(){
-      try {
+    public Value KeyFactory() throws Exception{
+
           SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
           KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
           keyGen.initialize(1024, random);
           KeyPair pair = keyGen.generateKeyPair();
           PrivateKey priv = pair.getPrivate();
           PublicKey pub = pair.getPublic();
-          Value response = Value.create();
-          response.getFirstChild(pub).setValue(priv);
-         } catch (Exception e) {
-           System.err.println("Caught exception " + e.toString());
-         }
-         return response;
+          Value risp = Value.create();
+          risp.getFirstChild("public").setValue(pub);
+          risp.getFirstChild("private").setValue(priv);
+          return risp;
     }
 }
